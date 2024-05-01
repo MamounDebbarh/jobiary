b0dbedd4cb4e5c935388761a6935f04dde01df59
MamounDebbarh <m.m.debbarh@gmail.com>
2024-05-01 18:10:16 +0100

    Dashboard Structure update

---
+++ my-app/components/DashMenu.tsx
@@ -0,0 +1,62 @@
+import {
+  Bell,
+  Home,
+  LineChart,
+  Package2,
+  Plus,
+  ShoppingCart,
+} from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Separator } from "./ui/separator";
+
+export default function DashMenu() {
+  return (
+    <>
+      <div className="hidden border-r bg-muted/40 md:block">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <Link href="/" className="flex items-center gap-2 font-semibold">
+              <Package2 className="h-6 w-6" />
+              <span className="">Jobiary</span>
+            </Link>
+            <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+              <Bell className="h-4 w-4" />
+              <span className="sr-only">Toggle notifications</span>
+            </Button>
+          </div>
+          <div className="flex-1">
+            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Home className="h-4 w-4" />
+                Goals
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <LineChart className="h-4 w-4" />
+                Analytics
+              </Link>
+              <Separator className="my-4" />
+
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <ShoppingCart className="h-4 w-4" />
+                Funnels
+                <Button size="icon" className="ml-auto h-4 w-4 rounded-full">
+                  <Plus />
+                </Button>
+              </Link>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}

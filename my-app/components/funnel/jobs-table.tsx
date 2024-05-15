6a9dd5b1baa61dad18869961021d5b30317135b5
MamounDebbarh <m.m.debbarh@gmail.com>
2024-05-15 11:59:35 +0100

    Added Jobs table.

---
+++ my-app/components/funnel/jobs-table.tsx
@@ -0,0 +1,97 @@
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import { Skeleton } from "../ui/skeleton";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+  isLoading: boolean;
+}
+
+export function JobsTable<TData, TValue>({
+  columns,
+  data,
+  isLoading,
+}: DataTableProps<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {isLoading ? (
+            <div className="space-y-3 my-5 ml-3">
+              <Skeleton className="h-3 w-2/3" />
+              <Skeleton className="h-3 w-3/4" />
+              <Skeleton className="h-3 w-3/4" />
+            </div>
+          ) : (
+            <>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
--- my-app/components/funnel/page.tsx
+++ my-app/components/funnel/page.tsx
@@ -1,15 +1,42 @@
-// components/FunnelPage.tsx
-import React from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "../ui/card";
+import { createClient } from "@/utils/supabase/client";
+import React, { useEffect, useState } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import Goal from "./goal";
 import JobForm from "./job-form";
+import { JobsTable } from "./jobs-table";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { z } from "zod";
+
+const JobSchema = z.object({
+  job_id: z.string(),
+  funnel_id: z.string(),
+  name: z.string(),
+  step: z.string(),
+  date: z.date(),
+  notes: z.string().optional(),
+});
+
+type Job = z.infer<typeof JobSchema>;
+
+const columns: ColumnDef<Job>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "step",
+    header: "Step",
+  },
+  {
+    accessorKey: "date",
+    header: "Date",
+  },
+  {
+    accessorKey: "notes",
+    header: "Notes",
+  },
+];
 
 interface FunnelPageProps {
   selectedFunnel: string;
@@ -20,6 +47,27 @@ const FunnelPage: React.FC<FunnelPageProps> = ({
   selectedFunnel,
   funnelID,
 }) => {
+  const [jobs, setJobs] = useState<Job[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const supabase = createClient();
+
+  useEffect(() => {
+    fetchJobs();
+  }, [funnelID]);
+
+  const fetchJobs = async () => {
+    setIsLoading(true); // Set isLoading to true when start fetching data
+    const { data, error } = await supabase
+      .from("jobs")
+      .select("*")
+      .eq("funnel_id", funnelID);
+
+    if (error) console.log("Error: ", error);
+    else setJobs(data);
+    setIsLoading(false); // Set isLoading to false when finish fetching data
+  };
+
   return (
     <div
       className="flex flex-col md:flex-row flex-1 justify-start h-screen rounded-lg border border-dashed shadow-sm"
@@ -28,14 +76,10 @@ const FunnelPage: React.FC<FunnelPageProps> = ({
       <Card className="m-4 mb-0 md:w-3/5 md:mb-4">
         <CardHeader>
           <CardTitle>{selectedFunnel}</CardTitle>
-          <CardDescription>Funnel data</CardDescription>
         </CardHeader>
         <CardContent>
-          <p>{funnelID}</p>
+          <JobsTable columns={columns} data={jobs} isLoading={isLoading} />
         </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
       </Card>
       <div className="flex flex-col justify-between m-4 space-y-4 md:w-2/5 md:mb-4">
         <JobForm funnelID={funnelID} />
